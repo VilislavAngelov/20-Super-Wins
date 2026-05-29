f74038d38a33ac795d077bcfbc346c273ecf6293
Vilislav Angelov <vilislavangelov@gmail.com>
2026-05-29 09:25:20 -0700

    Decided to make this a separate project. I initially started it in the python-for-ai project

---
+++ 20_super_wins.py
@@ -0,0 +1,114 @@
+from tkinter import *
+from tkinter import ttk
+import random
+
+
+def spin(balance):
+        symbols = ["🍒", "🍊", "🍉", "🍇", "👑", "🃏"]
+        row = []
+        
+        #Making the most basic version
+        for i in range(3):
+            row.append(random.choice(symbols))
+
+        if row == ["🍒", "🍒","🍒"]:
+            print(row)
+            print("You Win 30")
+            balance += 30
+            print(f"Balance ${balance}")
+        elif row == ["🍊", "🍊","🍊"]:
+            print(row)
+            print("You Win 40")
+            balance += 40
+            print(f"Balance ${balance}")
+        elif row == ["🍉", "🍉","🍉"]:
+            print(row)
+            print("You Win 80")
+            balance += 80
+            print(f"Balance ${balance}")
+        elif row == ["🍇", "🍇","🍇"]:
+            print(row)
+            print("You Win 160")
+            balance += 160
+            print(f"Balance ${balance}")
+        elif row == ["👑", "👑","👑"]:
+            print(row)
+            print("You Win 320")
+            balance += 320
+            print(f"Balance ${balance}")
+        elif row == ["🃏", "🃏","🃏"]:
+            print(row)
+            print("You Win 1000")
+            balance += 1000
+            print(f"Balance ${balance}")
+        else: 
+            print(row) 
+            print()
+            print(f"Balance ${balance}")
+        #how to make the algorithm
+        #how RNG works
+ 
+
+
+def main():
+ 
+    balance = 1000
+    bet = 10
+
+    print("##########################")
+    print("##### 20 SUPER WINS ######")
+    print("['🃏', '🃏', '🃏']")
+    print("##### Place Your Bet #####")
+    print(f"### Balance ${balance} ###")
+    while balance > bet:
+
+        balance -= bet
+        input("")
+        spin(balance) 
+
+
+    
+
+main()
+
+print("Insufficient Balance")
+
+'''
+root = Tk()
+root.title("Slot Machine")
+
+mainframe = ttk.Frame(root, padding=20)
+mainframe.grid(column=0, row=0, sticky=(N, W, E, S))
+
+ttk.Label(mainframe, text="20 SUPER WIN").grid(column=4, row=0, sticky=S)
+
+
+
+root.mainloop()
+'''
+
+'''
+TASK LIST
+1. Break down the task to smaller tasks
+    -How a slot machine works
+        I haven't found out this yet exactly
+    -How to display the reels
+    -Check if you have enough money to spin
+    -How to deduct spin from balance
+    -How to add wins to balance
+    -How to define the symbols
+    -How to make the slot algorithm
+    -How to display balance
+    -How to calculate wins
+    -Do I have to use OOP for a slot machine
+    -How to make the columns show the symbols one by one on the slot left to right
+    -How do I mmake the 20 SUPER WINS a title 
+    -How to define the lines
+    -How do I change the bet
+    -How do I spin by clicking spacebar
+    -How to position the widgets the way I want 
+    -How to make a symbol wild
+    -Calculate the RTP (Aim between 95 and 98 percent)
+but since it's not assigned to any variable,
+it is effectively ignored during execution.
+'''
