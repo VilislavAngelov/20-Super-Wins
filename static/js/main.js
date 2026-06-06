deb72a1a0878230f4e2a6a198249933ad1dee7ff
Vilislav Angelov <vilislavangelov@gmail.com>
2026-06-06 00:29:34 -0700

    The bet size buttons work on the front end now by deducting from the balance, but that doesnt actually change the bet amount on the back end so far. the thing is people can abuse how this works by using inspect element on a bet size button and betting  instead of  so in my opinion the bet sizes and the balance should be server side

--- static/js/main.js
+++ static/js/main.js
@@ -3,9 +3,16 @@ const balance = document.getElementById('balance');
 const winnings = document.getElementById('winnings');
 const cells = document.querySelectorAll('.symbol');
 let playerBalance = 1000;
-const BET = 10;
+let BET = 10;
+const bet_select = document.getElementById('bet-select')
 
 balance.textContent = 'Balance: $' + playerBalance;
+
+//This kind of works but can be abused because someone can use inspect on a bet amount, change it locally and then bet $800 instead of 80 for examle. I think bet sizes should be server size as well as the balance
+
+bet_select.addEventListener('click', function(e) {
+  BET = parseInt(e.target.innerText.replace('$', ''));
+})
 
 spin_button.addEventListener('click', async () => {
     if (playerBalance < BET) return;
