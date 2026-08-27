c5508e86e547c16ae5384665827a0dc9e787aa23
Vilislav Angelov <vilislavangelov@gmail.com>
2026-08-27 09:30:56 -0700

    used WAAPI to create a seamless animation from the beginning to the end of the spin, no matter if you leave it to unfold or stop it via the stop button you don't see any jumps or symbols changing in the viewport.

--- static/js/main.js
+++ static/js/main.js
@@ -7,7 +7,7 @@ const reels = document.querySelectorAll('.reel');
 const bet_select = document.getElementById('bet-select');
 const reset_balance = document.getElementById('reset-balance');
 let bet_buttons = document.getElementsByClassName("bet-amount");
-const SYMBOLS = ["🍋","🍒","🍊","🍉","🍇","👑","🃏","⭐"];
+const SYMBOLS = ["🍋", "🍒", "🍊", "🍉", "🍇", "👑", "🃏", "⭐"];
 let landing_sound0 = new Audio("/static/sounds/land.wav");
 let landing_sound1 = new Audio("/static/sounds/land.wav");
 let landing_sound2 = new Audio("/static/sounds/land.wav");
@@ -17,21 +17,22 @@ let landing_sounds = [landing_sound0, landing_sound1, landing_sound2, landing_so
 let interval = 2000;
 let spin_result = null
 let animations = []
+let landings = []
 let last_lap = [0, 0, 0, 0, 0]
-let spin_iterations = 2
+let spin_data = null
+let laps_after_result = [0, 0, 0, 0, 0]
+let stopping = false
+let landed = 0
 
-
-
-
-for(let i = 0; i < bet_buttons.length; i++){
+for (let i = 0; i < bet_buttons.length; i++) {
     bet_buttons[i].addEventListener('click', async (e) => {
-    await fetch('/bet', {
-        method: 'POST',
-        headers: {'Content-Type': 'application/json'},
-        body: JSON.stringify({bet_size: Number(e.target.dataset.bet)})
+        await fetch('/bet', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ bet_size: Number(e.target.dataset.bet) })
+        })
+        doSpin()
     })
-    doSpin()
-})
 }
 
 async function loadState() {
@@ -42,95 +43,96 @@ async function loadState() {
     winnings.textContent = 'Last Win: $' + data.last_win;
 }
 
-// I need to make this function spin infinitely until we get a response from the /spin api and a response code 
-// we have to select the screen and insert it in the infinitely spinning animation 
+// I need to make this function spin infinitely until we get a response from the /spin api and a response code
+// we have to select the screen and insert it in the infinitely spinning animation
 function spin_animation() {
-    strips.forEach((strip,reel) => {
+    strips.forEach((strip, reel) => {
         const symbols = strip.querySelectorAll('.symbol')
         const tiles = (symbols.length - 3)
-        let travel =  tiles * symbols[0].offsetHeight
-        let duration = tiles * 100
-        
-        symbols[symbols.length - 3].textContent = symbols[0].textContent
-        symbols[symbols.length - 2].textContent = symbols[1].textContent
-        symbols[symbols.length - 1].textContent = symbols[2].textContent
+        const travel = tiles * symbols[0].offsetHeight
 
-        for (let i = 3; i < symbols.length - 3; i++) {
-                symbols[i].textContent = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
-            } 
-        
+        for (let i = 0; i < symbols.length - 3; i++) {
+            symbols[i].textContent = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
+        }
+
         animations[reel] = strip.animate(
             [
-                { transform: `translateY(-${travel}px)`},
+                { transform: `translateY(-${travel}px)` },
                 { transform: "translateY(0)" }
             ],
             {
-                duration: duration,
+                duration: travel,
                 iterations: Infinity
             }
         )
     })
 }
 
-async function doSpin() {
-    spin_result = null
-    last_lap = [0, 0, 0, 0, 0]
-    spin_iterations = 2
-    change_spin_state('WAITING')
-    spin_animation()
-    const response = await fetch('/spin', {method: "POST"});
-    const data = await response.json();
-    
-
-    if (response.ok){
-        change_spin_state('SPINNING')
-        balance.textContent = 'Balance: $' + data.spin_balance;
-        function value() {
-            let startValue = 0,
-                endValue = data.winnings,
-                duration = Math.floor(interval / endValue);
-            let counter = setInterval(function () {
-                    startValue += 1;
-                    winnings.textContent = 'Winnings: $' + startValue;
-                    if (startValue >= endValue){
-                        clearInterval(counter);
-                    }
-                }, duration);
+function value() {
+    let startValue = 0,
+        endValue = spin_data.winnings,
+        duration = Math.floor(interval / endValue);
+    let counter = setInterval(function () {
+        startValue += 1;
+        winnings.textContent = 'Winnings: $' + startValue;
+        if (startValue >= endValue) {
+            clearInterval(counter);
         }
+    }, duration);
+}
 
-        strips[strips.length - 1].addEventListener("animationend", (strip) => {
-            change_spin_state('IDLE')
-            if (data.winnings > 0) {
-                value();
-                balance.textContent = 'Balance: $' + data.balance;
-            } else {
-                winnings.textContent = 'Last Win: $' + data.last_win
-                balance.textContent = 'Balance: $' + data.balance;
-            }
-        }, { once: true })  
-        
-        spin_result = data.screen 
+function display_wins() {
+    change_spin_state('IDLE')
+    if (spin_data.winnings > 0) {
+        value();
+        balance.textContent = 'Balance: $' + spin_data.balance;
+    } else {
+        winnings.textContent = 'Last Win: $' + spin_data.last_win
+        balance.textContent = 'Balance: $' + spin_data.balance;
     }
 }
 
-spin_button.addEventListener('click', async () => {  
+
+
+async function doSpin() {
+    spin_result = null
+    last_lap = [0, 0, 0, 0, 0]
+    stopping = false
+    landed = 0
+    laps_after_result = [0, 0, 0, 0, 0]
+    change_spin_state('WAITING')
+    spin_animation()
+    const response = await fetch('/spin', { method: "POST" });
+    const data = await response.json();
+    spin_data = data
+
+    if (response.ok) {
+        change_spin_state('SPINNING')
+        balance.textContent = 'Balance: $' + data.spin_balance;
+
+        spin_result = data.screen
+    }
+}
+
+spin_button.addEventListener('click', async () => {
     doSpin()
 })
 
-stop_button.addEventListener('click', async () => {  
-    spin_iterations = 0
-    stop_button.disabled = true;
+stop_button.addEventListener('click', () => {
+    if (spin_result === null) return
+    stopping = true
+    stop_button.disabled = true
 })
 
 reset_balance.addEventListener('click', async () => {
-    const response = await fetch('/reset-balance', {method: "POST"});
+    const response = await fetch('/reset-balance', { method: "POST" });
     const data = await response.json();
 
     balance.textContent = 'Balance: $' + data
 })
 
 function change_spin_state(spin_state) {
-    switch(spin_state) {
+    switch (spin_state) {
         case 'IDLE':
             spin_button.disabled = false;
             spin_button.style.display = 'block';
@@ -152,17 +154,57 @@ function change_spin_state(spin_state) {
     }
 }
 
+function land(reel) {
+    const strip = strips[reel]
+    const symbols = strip.querySelectorAll('.symbol')
+    const travel = (symbols.length - 3) * symbols[0].offsetHeight
+
+    animations[reel].cancel()
+
+    landings[reel] = strip.animate(
+        [
+            { transform: `translateY(-${travel}px)` },
+            { transform: "translateY(0)" }
+        ],
+        { duration: travel, easing: 'linear' }
+    )
+
+    landings[reel].finished.then(() => {
+        landing_sounds[reel].currentTime = 0
+        landing_sounds[reel].play().catch(() => { })
+
+        symbols[symbols.length - 3].textContent = symbols[0].textContent
+        symbols[symbols.length - 2].textContent = symbols[1].textContent
+        symbols[symbols.length - 1].textContent = symbols[2].textContent
+
+        landed++
+        if (landed === strips.length) display_wins()
+    })
+}
+
 function watch_animation() {
     animations.forEach((animation, reel) => {
         const symbols = strips[reel].querySelectorAll('.symbol')
         let lap = Math.floor(animation.currentTime / animation.effect.getTiming().duration)
-        if (lap !== last_lap[reel]) {
+
+        if (animation.playState == 'idle') {
+
+        } else if (lap !== last_lap[reel]) {
             symbols[symbols.length - 3].textContent = symbols[0].textContent
             symbols[symbols.length - 2].textContent = symbols[1].textContent
             symbols[symbols.length - 1].textContent = symbols[2].textContent
 
-            for (let i = 3; i < symbols.length - 3; i++) {
-                symbols[i].textContent = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]    
+            if (spin_result !== null) laps_after_result[reel] += 1
+
+            if (spin_result !== null && (stopping || laps_after_result[reel] > reel)) {
+                for (let i = 0; i < 3; i++) {
+                    symbols[i].textContent = spin_result[reel][i]
+                }
+                land(reel)
+            } else {
+                for (let i = 0; i < 3; i++) {
+                    symbols[i].textContent = SYMBOLS[Math.floor(Math.random() * SYMBOLS.length)]
+                }
             }
         }
         last_lap[reel] = lap
